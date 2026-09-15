42b2dbd2f14392b880158436d38717d4c639f7b2
nimkibet <nimrodkibet376@gmail.com>
2026-09-15 10:41:10 +0300

    style: move Expense Entry Card below Ledger table

--- check_bp.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('pos_fresh_inventory.db', sqlite3.OPEN_READONLY);
-
-db.all("SELECT name, buying_price, selling_price FROM inventory ORDER BY name LIMIT 20", (err, rows) => {
-    if(err) return console.error(err);
-    console.log('SQLite sample with buying prices:');
-    rows.forEach(r => console.log(`  ${r.name} | BP: ${r.buying_price} | SP: ${r.selling_price}`));
-    db.close();
-});
